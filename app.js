920a0f7597db1d7bebe8084e0bfc4bdd28700d1a
DianaB177 <anaidb77@gmail.com>
2022-11-16 11:54:57 +0100

    numero par

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 'Use strict';
+// Suma
+let valor_1 = 2;
+let valo_2 = 3;
+
+console.log(valor_1 + valo_2);
+
+// Crea una función que determine si un número es par.
+
+let valor_3 =parseInt(prompt(`Escribe una cifra para saber si es par o impar`));
+
+if (valor_3 % 2==0) {
+    alert(`El número ${valor_3} es par`)
+} else {
+    alert(`El número ${valor_3} es impar`)
+}
+
