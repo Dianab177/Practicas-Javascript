cfddc6109f03a41967791dd568b99911e982b027
DianaB177 <anaidb77@gmail.com>
2022-11-17 17:54:11 +0100

    pract

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 'Use strict';
 // Suma
-/*let valor_1 = 2;
-let valo_2 = 3;
-
-console.log(valor_1 + valo_2);
+/*
+function sumar(a, b) {
+    let resultado = a + b;
+    console.log(resultado);
+    return resultado;
+}sumar(2, 6);
 */
+
 // Crea una función que determine si un número es par.
 
 /*let valor_3 =parseInt(prompt(`Escribe una cifra para saber si es par o impar`));
@@ -31,39 +34,85 @@ console.log(carro.puertas);
 console.log(carro.caracteristicas.color);
 */
 //Array con diez números.
-let numeros = [0, 1, 2, 3, 4, 5, 6,7, 8, 9];
+//let numeros = [0, 1, 2, 3, 4, 5, 6,7, 8, 9];
 //For
 /*
-for (let num = 0; num < array.length; num ++) {
+for (let num = 0; num < numeros.length; num ++) {
     const element = numeros [num];
     console.log(element);
 }
+*/
 /*
 for (let num = 0; num < 10; num++) {
     console.log(num);
 }
 */
-void eliminaPares(){
-    int tamanho=vector.length;
-    for(int i=0;i<vector.length;i++){
-       if(i%2==1){
-          for (int j=i;j<vector.length;j++)
-             vector[j]=vector[j+1];
-       }
-    }
- }
- let numero_pares = numeros.filter(function (numeros) {
-    return numeros % != 2
+let data = [0, 1, 2, 3, 4, 5, 6,, 6, 7, 7, 8, 9];
 
- let numero_pares = numeros.filter(function (numero) {
+/*let numero_pares = data.filter(function (numero) {
     return numero % 2 == 0;
  });
- console.log(numero_pares); console.log(numero_pares);
- let numerosOrdenados = numeros.findIndex(numero_pares);
- console.log(numerosOrdenados);
+ console.log(numero_pares);
+*/
+/*
+let result = data.reduce((acc, numero_pares)=>{
+   if(!acc.includes(numero_pares)){
+    acc.push(numero_pares);
+   }
+   return acc;
+},[]);
 
- if (numeros.includes([numero_pares])) {
-    delete numeros.numero_pares
- }
+console.log(result);
+*/
+for (let num = 0; num < data.length; num++) {
+    const element = data[num];
+    if (num % 2==0) {
+        let mostrar = data.indexOf(element);
+        data.splice(0, 9, mostrar);
+        console.log(mostrar);
+    } 
+}
 
-console.log(numeros);
+
+/*
+let max = numeros.reduce(function(a, b) {
+    return Math.max(a, b);
+}, -Infinity);
+console.log(max);
+let min = numeros.reduce(function(a, b) {
+    return Math.min(a, b);
+}, -Infinity);
+console.log(min)
+let x = Math.min([numeros]);
+console.log(x);
+
+console.log( "alphabet".toUpperCase() );
+console.log( "alphabet".toLowerCase() );
+
+let nombre = [{name:'maria'},{name:'josefina'} ,{name: 'teresita'}];
+nombre.forEach(i => {
+    // reemplazar total_kills_
+    i.name = i.name.replace("total_kills_","");
+    // i.name es igual al primer elemento en mayúscula más el 
+    // resto de la palabra
+    i.name = i.name[0].toUpperCase() + i.name.slice(1);
+  });
+  
+  console.log(nombre);
+*/
+let suNombre = [
+    {nombres: 'Lola'},
+    {nombres: 'Josefa'},
+    {nombres: 'Pepa'},
+    {nombres: 'Manola'},
+    {nombres: 'Pepina'},
+    {nombres: 'Maria'},
+    {nombres: 'Rosa'},
+    {nombres: 'Silda'},
+    {nombres: 'Silvia'},
+    {nombres: 'Carla'},
+]
+console.log(suNombre);
+
+let numbers = [0, 1, 2, 3, 4, 5, 6,7, 8, 9];
+numbers.forEach(element => console.log(element));
